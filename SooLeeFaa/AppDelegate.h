fe6929c8b26277b5cc50fa70ae8f273fe7c6ee21
Almas <32594114+zainoldin@users.noreply.github.com>
2019-01-05 00:16:40 +0600

    feat: add project

---
+++ SooLeeFaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SooLeeFaa
+//
+//  Created by Almas on 1/4/19.
+//  Copyright © 2019 Almas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SooLeeFaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SooLeeFaa
+//
+//  Created by Almas on 1/4/19.
+//  Copyright © 2019 Almas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
